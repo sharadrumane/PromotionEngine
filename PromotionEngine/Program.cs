ef89da8c1c90331d0a093b8d9ca454fa8be3feff
sharadrumane <62588956+sharadrumane@users.noreply.github.com>
2020-08-31 21:04:53 +0530

    Update Program.cs

--- PromotionEngine/Program.cs
+++ PromotionEngine/Program.cs
@@ -6,7 +6,19 @@ namespace PromotionEngine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> products = new List<Product>();
+
+            Console.WriteLine("total number of order");
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine("enter the type of product:A,B,C or D");
+                string type = Console.ReadLine();
+                Product p = new Product();
+                products.Add(p);
+            }
+
+            Console.ReadLine();
         }
     }
 }
