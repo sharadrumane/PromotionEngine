576a51b5cacb80ca398182f495b9fb2c5278f960
sharadrumane <62588956+sharadrumane@users.noreply.github.com>
2020-08-31 21:07:31 +0530

    Update Product.cs

--- PromotionEngine/Product.cs
+++ PromotionEngine/Product.cs
@@ -9,5 +9,31 @@ namespace PromotionEngine
         public string Id { get; set; }
         public decimal Price { get; set; }
         public int Value { get; set; }
+        
+        public Product(string id, int value)
+        {
+            this.Id = id;
+            this.Value = value;
+            switch (id)
+            {
+                case "A":
+                    this.Price = 50m;
+
+                    break;
+                case "B":
+                    this.Price = 30m;
+
+                    break;
+                case "C":
+                    this.Price = 20m;
+
+                    break;
+                case "D":
+                    this.Price = 15m;
+                    break;
+            }
+        }
     }
+    
+    
 }
