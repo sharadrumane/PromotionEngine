9a85e4fbe30a41df2c5a5824d42b6e18b2f5f90f
sharadrumane <62588956+sharadrumane@users.noreply.github.com>
2020-08-31 20:44:13 +0530

    Add files via upload

---
+++ PromotionEngine/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public decimal Price { get; set; }
+        public int Value { get; set; }
+    }
+}
