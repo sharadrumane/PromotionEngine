a1de481ecd5c0620e784e3dd15262225ca6be68d
sharadrumane <62588956+sharadrumane@users.noreply.github.com>
2020-08-31 21:28:44 +0530

    Update Product.cs

--- PromotionEngine/Product.cs
+++ PromotionEngine/Product.cs
@@ -49,5 +49,49 @@ namespace PromotionEngine
 
             return Value;
         }
+
+        public static int GetTotalPrice(List<Product> products)
+        {
+            int counterofA = 0;
+            int priceofA = 50;
+            int counterofB = 0;
+            int priceofB = 30;
+            int CounterofC = 0;
+            int priceofC = 20;
+            int CounterofD = 0;
+            int priceofD = 15;
+
+            if (products.Count == 4)
+            {
+                priceofC = 0;
+            }
+
+            foreach (Product pr in products)
+            {
+                if (pr.Id == "A" || pr.Id == "a")
+                {
+                    counterofA = pr.Value;
+                }
+                if (pr.Id == "B" || pr.Id == "b")
+                {
+                    counterofB = pr.Value;
+                }
+                if (pr.Id == "C" || pr.Id == "c")
+                {
+                    CounterofC = pr.Value;
+                }
+                if (pr.Id == "D" || pr.Id == "d")
+                {
+                    CounterofD = pr.Value;
+                    CounterofD = CounterofD + 1;
+                }
+            }
+            int totalPriceofA = (counterofA / 3) * 130 + (counterofA % 3 * priceofA);
+            int totalPriceofB = (counterofB / 2) * 45 + (counterofB % 2 * priceofB);
+            int totalPriceofC = (CounterofC * priceofC);
+            int totalPriceofD = (CounterofD * priceofD);
+            return totalPriceofA + totalPriceofB + totalPriceofC + totalPriceofD;
+
+        }
     }
 }
